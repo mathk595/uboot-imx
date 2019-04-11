2be03252cd7049a5b5e197766cdb3a8759f4e644
Sven Hillger <hillger@keith-koep.com>
2019-04-11 15:51:20 +0200

    Added Trizeps VIII support.
    (cherry picked from commit ea0e2e7f90a1a0f1ad108c47a9a6f0b848e1d699)

---
+++ board/keithkoep/trizeps8/spl.c
@@ -0,0 +1,545 @@
+/*
+ * Copyright 2017 NXP
+ *
+ * SPDX-License-Identifier:	GPL-2.0+
+ */
+
+#include <common.h>
+#include <spl.h>
+#include <asm/io.h>
+#include <errno.h>
+#include <asm/io.h>
+#include <asm/mach-imx/iomux-v3.h>
+#include <asm/arch/ddr.h>
+#include <asm/arch/imx8mq_pins.h>
+#include <asm/arch/sys_proto.h>
+#include <power/pmic.h>
+#include <power/pfuze100_pmic.h>
+#include "../common/pfuze.h"
+#include <asm/arch/clock.h>
+#include <asm/mach-imx/gpio.h>
+#include <asm/mach-imx/mxc_i2c.h>
+#include <fsl_esdhc.h>
+#include <mmc.h>
+#include <asm/arch/imx8m_ddr.h>
+
+DECLARE_GLOBAL_DATA_PTR;
+
+extern struct dram_timing_info lpddr4_timing_20190411;
+
+void spl_dram_init(void)
+{
+	/* ddr init */
+	if ((get_cpu_rev() & 0xfff) == CHIP_REV_2_1)
+		ddr_init(&lpddr4_timing_20190411);
+	else
+		ddr_init(&lpddr4_timing_20190411);
+}
+
+#define I2C_PAD_CTRL	(PAD_CTL_DSE6 | PAD_CTL_HYS | PAD_CTL_PUE)
+#define PC MUX_PAD_CTRL(I2C_PAD_CTRL)
+struct i2c_pads_info i2c_pad_info3 = {
+	.scl = {
+		.i2c_mode = IMX8MQ_PAD_I2C3_SCL__I2C3_SCL | PC,
+		.gpio_mode = IMX8MQ_PAD_I2C3_SCL__GPIO5_IO18 | PC,
+		.gp = IMX_GPIO_NR(5, 14),
+	},
+	.sda = {
+		.i2c_mode = IMX8MQ_PAD_I2C3_SDA__I2C3_SDA | PC,
+		.gpio_mode = IMX8MQ_PAD_I2C3_SDA__GPIO5_IO19 | PC,
+		.gp = IMX_GPIO_NR(5, 15),
+	},
+};
+
+#define USDHC2_CD_GPIO	IMX_GPIO_NR(2, 12)
+#define USDHC1_PWR_GPIO IMX_GPIO_NR(2, 10)
+#define USDHC2_PWR_GPIO IMX_GPIO_NR(2, 19)
+
+int board_mmc_getcd(struct mmc *mmc)
+{
+	struct fsl_esdhc_cfg *cfg = (struct fsl_esdhc_cfg *)mmc->priv;
+	int ret = 0;
+
+	switch (cfg->esdhc_base) {
+	case USDHC1_BASE_ADDR:
+		ret = 1;
+		break;
+	case USDHC2_BASE_ADDR:
+		ret = 1; // Ignore Card-Detect-GPIO!gpio_get_value(USDHC2_CD_GPIO);
+		return ret;
+	}
+
+	return 1;
+}
+
+#define USDHC_PAD_CTRL	(PAD_CTL_DSE6 | PAD_CTL_HYS | PAD_CTL_PUE | \
+			 PAD_CTL_FSEL2)
+#define USDHC_GPIO_PAD_CTRL (PAD_CTL_PUE | PAD_CTL_DSE1)
+
+static iomux_v3_cfg_t const usdhc1_pads[] = {
+	IMX8MQ_PAD_SD1_CLK__USDHC1_CLK | MUX_PAD_CTRL(USDHC_PAD_CTRL),
+	IMX8MQ_PAD_SD1_CMD__USDHC1_CMD | MUX_PAD_CTRL(USDHC_PAD_CTRL),
+	IMX8MQ_PAD_SD1_DATA0__USDHC1_DATA0 | MUX_PAD_CTRL(USDHC_PAD_CTRL),
+	IMX8MQ_PAD_SD1_DATA1__USDHC1_DATA1 | MUX_PAD_CTRL(USDHC_PAD_CTRL),
+	IMX8MQ_PAD_SD1_DATA2__USDHC1_DATA2 | MUX_PAD_CTRL(USDHC_PAD_CTRL),
+	IMX8MQ_PAD_SD1_DATA3__USDHC1_DATA3 | MUX_PAD_CTRL(USDHC_PAD_CTRL),
+	IMX8MQ_PAD_SD1_DATA4__USDHC1_DATA4 | MUX_PAD_CTRL(USDHC_PAD_CTRL),
+	IMX8MQ_PAD_SD1_DATA5__USDHC1_DATA5 | MUX_PAD_CTRL(USDHC_PAD_CTRL),
+	IMX8MQ_PAD_SD1_DATA6__USDHC1_DATA6 | MUX_PAD_CTRL(USDHC_PAD_CTRL),
+	IMX8MQ_PAD_SD1_DATA7__USDHC1_DATA7 | MUX_PAD_CTRL(USDHC_PAD_CTRL),
+	IMX8MQ_PAD_SD1_RESET_B__GPIO2_IO10 | MUX_PAD_CTRL(NO_PAD_CTRL),
+};
+
+static iomux_v3_cfg_t const usdhc2_pads[] = {
+	IMX8MQ_PAD_SD2_CLK__USDHC2_CLK | MUX_PAD_CTRL(USDHC_PAD_CTRL), /* 0xd6 */
+	IMX8MQ_PAD_SD2_CMD__USDHC2_CMD | MUX_PAD_CTRL(USDHC_PAD_CTRL), /* 0xd6 */
+	IMX8MQ_PAD_SD2_DATA0__USDHC2_DATA0 | MUX_PAD_CTRL(USDHC_PAD_CTRL), /* 0xd6 */
+	IMX8MQ_PAD_SD2_DATA1__USDHC2_DATA1 | MUX_PAD_CTRL(USDHC_PAD_CTRL), /* 0xd6 */
+	IMX8MQ_PAD_SD2_DATA2__USDHC2_DATA2 | MUX_PAD_CTRL(USDHC_PAD_CTRL), /* 0x16 */
+	IMX8MQ_PAD_SD2_DATA3__USDHC2_DATA3 | MUX_PAD_CTRL(USDHC_PAD_CTRL), /* 0xd6 */
+	IMX8MQ_PAD_SD2_CD_B__GPIO2_IO12 | MUX_PAD_CTRL(USDHC_GPIO_PAD_CTRL),
+//	IMX8MQ_PAD_SD2_RESET_B__GPIO2_IO19 | MUX_PAD_CTRL(USDHC_GPIO_PAD_CTRL),	Trizeps8: used as PCIE1_\RESET.
+};
+
+static struct fsl_esdhc_cfg usdhc_cfg[2] = {
+	{USDHC1_BASE_ADDR, 0, 4},
+	{USDHC2_BASE_ADDR, 0, 4},
+};
+
+int board_mmc_init(bd_t *bis)
+{
+	int i, ret;
+	/*
+	 * According to the board_mmc_init() the following map is done:
+	 * (U-Boot device node)    (Physical Port)
+	 * mmc0                    USDHC1
+	 * mmc1                    USDHC2
+	 */
+	for (i = 0; i < CONFIG_SYS_FSL_USDHC_NUM; i++) {
+		switch (i) {
+		case 0:
+			usdhc_cfg[0].sdhc_clk = mxc_get_clock(USDHC1_CLK_ROOT);
+			imx_iomux_v3_setup_multiple_pads(
+				usdhc1_pads, ARRAY_SIZE(usdhc1_pads));
+			gpio_request(USDHC1_PWR_GPIO, "usdhc1_reset");
+			gpio_direction_output(USDHC1_PWR_GPIO, 0);
+			udelay(500);
+			gpio_direction_output(USDHC1_PWR_GPIO, 1);
+			break;
+		case 1:
+			usdhc_cfg[1].sdhc_clk = mxc_get_clock(USDHC2_CLK_ROOT);
+			imx_iomux_v3_setup_multiple_pads(
+				usdhc2_pads, ARRAY_SIZE(usdhc2_pads));
+/* Trizeps8: Use as PCIE1_\RESET; usdhc2_reset not used!	
+			gpio_request(USDHC2_PWR_GPIO, "usdhc2_reset");
+			gpio_direction_output(USDHC2_PWR_GPIO, 0);
+			udelay(500);
+			gpio_direction_output(USDHC2_PWR_GPIO, 1);
+*/
+			break;
+		default:
+			printf("Warning: you configured more USDHC controllers"
+				"(%d) than supported by the board\n", i + 1);
+			return -EINVAL;
+		}
+
+		ret = fsl_esdhc_initialize(bis, &usdhc_cfg[i]);
+		if (ret)
+			return ret;
+	}
+
+	return 0;
+}
+
+#ifdef CONFIG_POWER
+#define I2C_PMIC	2	/* I2C3 */
+int power_init_board(void)
+{
+//	We got two PMIC:
+/*
+	U700 PMFP0100
+	
+	VGEN1&2:	VDD_FPGA_CORE		:	1.2V		FPGA Core
+	VGEN3:		VDD_PHY_1V8		:	1.8V		HDMI-Phy, Mipi		
+	VGEN4:		VDDA_1V8		:	1.8V		i.MX8M
+	VGEN5:		VDD_FPGA_MIPI		:	2.5V		FPGA IO
+	VGEN6:		VDD_SD1			:	1.8V - 3.3V	SD-Card/eMMC Voltage
+	
+	SW1AB:		VDD_ARM			: 	0.81V - 1.05V	(0.9V @1GHz; 1.0V >1GHz)
+	SW1C:		VDD_GPU			: 	0.81V - 1.05V	(0.9V @800MHz; 1.0V >800MHz)
+	SW2:		NVCC_DRAM		:	1.1V		LPDDR4
+	SW3A:		VDD_DRAM		:	0.81V - 1.05V	(0.9V @800MHz; 1.0V >800MHz)
+	SW4:		+1V8			:	1.8V
+	SWBST:		+5V			:	5.0V
+*/	
+/*
+	U701 FAN5355
+	SW:		VDD_SOC			:	0.9V		( SOC, VDDA, VPU, PHY)
+*/
+	struct pmic *p;
+	int ret;
+	unsigned int reg;
+
+	ret = power_pfuze100_init(I2C_PMIC);
+	if (ret)
+		return -ENODEV;
+
+	p = pmic_get("PFUZE100");
+	ret = pmic_probe(p);
+	if (ret)
+		return -ENODEV;
+
+	pmic_reg_read(p, PFUZE100_DEVICEID, &reg);
+	printf("PMIC:  PFUZE100 ID=0x%02x\n", reg);
+
+#if 0
+	// Set VDD_DRAM to 1.0V to support 1.6GHz:
+	// see comment in i.MX8M EVK schematic
+	pmic_reg_read(p, PFUZE100_SW3AVOL, &reg);
+	if ((reg & 0x3f) != 0x18) {
+		reg &= ~0x3f;
+		reg |= 0x18;
+		pmic_reg_write(p, PFUZE100_SW3AVOL, reg);
+	}
+#endif
+	ret = pfuze_mode_init(p, APS_PFM);
+	if (ret < 0)
+		return ret;
+
+	/* set SW3A standby mode to off */
+	pmic_reg_read(p, PFUZE100_SW3AMODE, &reg);
+	reg &= ~0xf;
+	reg |= APS_OFF;
+	pmic_reg_write(p, PFUZE100_SW3AMODE, reg);
+
+	return 0;
+}
+#endif
+
+/* ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
+#if 0
+#define PRCSS_CMP	64
+void spl_domemory_test(void)
+{
+	__u32* pmem;
+	__u32 cnt;
+	__u32 failcnt;
+	__u8* p8mem;
+	__u32 prcss;
+	__u32 prcss_cmp;
+
+	failcnt = 0;
+	printf("Checking %d bytes of memory\r\n", PHYS_SDRAM_SIZE);
+
+	cnt = PRCSS_CMP;	
+	while( cnt--)
+		puts(".");
+	puts("Fill u32ptr = memaddress\r\n");
+	pmem = (__u32*)CONFIG_SYS_SDRAM_BASE;
+	cnt = PHYS_SDRAM_SIZE/sizeof(__u32);
+	prcss_cmp = cnt / PRCSS_CMP;
+	prcss = 0;
+	while(cnt--)
+	{
+		*pmem++ = (__u32)pmem;
+		if ( prcss++ > prcss_cmp)
+		{
+			prcss = 0;
+			puts(".");
+		}	
+	}	
+	puts("Check:\r\n");
+	pmem = (__u32*)CONFIG_SYS_SDRAM_BASE;
+	cnt = PHYS_SDRAM_SIZE/sizeof(__u32);
+	prcss_cmp = cnt / PRCSS_CMP;
+	prcss = 0;
+	while(cnt--)
+	{
+		if (*pmem != (__u32)pmem)
+		{
+			failcnt++;
+			printf("0x%x is 0x%x; should 0x%x\r\n", (__u32)pmem, *pmem, (__u32)pmem);
+		}
+		pmem++;
+		if ( prcss++ > prcss_cmp)
+		{
+			prcss = 0;
+			puts(".");
+		}	
+	}
+	if ( failcnt)
+		printf("Test failed: %d errors\r\n", failcnt);
+	else
+		puts("Test success\r\n");
+	failcnt = 0;
+		
+	puts("Fill u32ptr = ~memaddress\r\n");
+	pmem = (__u32*)CONFIG_SYS_SDRAM_BASE;
+	cnt = PHYS_SDRAM_SIZE/sizeof(__u32);
+	prcss_cmp = cnt / PRCSS_CMP;
+	prcss = 0;
+	while(cnt--)
+	{
+		*pmem++ = ~(__u32)pmem;
+		if ( prcss++ > prcss_cmp)
+		{
+			prcss = 0;
+			puts(".");
+		}	
+	}	
+	puts("Check:\r\n");
+	pmem = (__u32*)CONFIG_SYS_SDRAM_BASE;
+	cnt = PHYS_SDRAM_SIZE/sizeof(__u32);
+	prcss_cmp = cnt / PRCSS_CMP;
+	prcss = 0;
+	while(cnt--)
+	{
+		if (*pmem != ~(__u32)pmem)
+		{
+			failcnt++;
+			printf("0x%x is 0x%x; should 0x%x\r\n", (__u32)pmem, *pmem, ~(__u32)pmem);
+		}
+		pmem++;
+		if ( prcss++ > prcss_cmp)
+		{
+			prcss = 0;
+			puts(".");
+		}	
+	}
+	if ( failcnt)
+		printf("Test failed: %d errors\r\n", failcnt);
+	else
+		puts("Test success\r\n");
+	failcnt = 0;
+
+	puts("Fill u8ptr = memaddress\r\n");
+	p8mem = (__u8*)CONFIG_SYS_SDRAM_BASE;
+	cnt = PHYS_SDRAM_SIZE/sizeof(__u8);
+	prcss_cmp = cnt / PRCSS_CMP;
+	prcss = 0;
+	while(cnt--)
+	{
+		*p8mem++ = (__u8)p8mem;
+		if ( prcss++ > prcss_cmp)
+		{
+			prcss = 0;
+			puts(".");
+		}	
+	}	
+	puts("Check:\r\n");
+	p8mem = (__u8*)CONFIG_SYS_SDRAM_BASE;
+	cnt = PHYS_SDRAM_SIZE/sizeof(__u8);
+	prcss_cmp = cnt / PRCSS_CMP;
+	prcss = 0;
+	while(cnt--)
+	{
+		if (*p8mem != (__u8)p8mem)
+		{
+			failcnt++;
+			printf("0x%x is 0x%x; should 0x%x\r\n", (__u8)p8mem, *p8mem, (__u8)p8mem);
+		}
+		p8mem++;
+		if ( prcss++ > prcss_cmp)
+		{
+			prcss = 0;
+			puts(".");
+		}	
+	}
+	if ( failcnt)
+		printf("Test failed: %d errors\r\n", failcnt);
+	else
+		puts("Test success\r\n");
+	failcnt = 0;
+
+	puts("Fill u32ptr = 0xFFFFFFFF\r\n");
+	pmem = (__u32*)CONFIG_SYS_SDRAM_BASE;
+	cnt = PHYS_SDRAM_SIZE/sizeof(__u32);
+	prcss_cmp = cnt / PRCSS_CMP;
+	prcss = 0;
+	while(cnt--)
+	{
+		*pmem++ = (__u32)0xFFFFFFFF;
+		if ( prcss++ > prcss_cmp)
+		{
+			prcss = 0;
+			puts(".");
+		}	
+	}	
+	puts("Check:\r\n");
+	pmem = (__u32*)CONFIG_SYS_SDRAM_BASE;
+	cnt = PHYS_SDRAM_SIZE/sizeof(__u32);
+	prcss_cmp = cnt / PRCSS_CMP;
+	prcss = 0;
+	while(cnt--)
+	{
+		if (*pmem != (__u32)0xFFFFFFFF)
+		{
+			failcnt++;
+			printf("0x%x is 0x%x; should 0x%x\r\n", (__u32)pmem, *pmem, (__u32)0xFFFFFFFF);
+		}
+		pmem++;
+		if ( prcss++ > prcss_cmp)
+		{
+			prcss = 0;
+			puts(".");
+		}	
+	}
+	if ( failcnt)
+		printf("Test failed: %d errors\r\n", failcnt);
+	else
+		puts("Test success\r\n");
+	failcnt = 0;
+
+	puts("Fill u32ptr = 0x0\r\n");
+	pmem = (__u32*)CONFIG_SYS_SDRAM_BASE;
+	cnt = PHYS_SDRAM_SIZE/sizeof(__u32);
+	prcss_cmp = cnt / PRCSS_CMP;
+	prcss = 0;
+	while(cnt--)
+	{
+		*pmem++ = (__u32)0;
+		if ( prcss++ > prcss_cmp)
+		{
+			prcss = 0;
+			puts(".");
+		}	
+	}	
+	puts("Check:\r\n");
+	pmem = (__u32*)CONFIG_SYS_SDRAM_BASE;
+	cnt = PHYS_SDRAM_SIZE/sizeof(__u32);
+	prcss_cmp = cnt / PRCSS_CMP;
+	prcss = 0;
+	while(cnt--)
+	{
+		if (*pmem != (__u32)0)
+		{
+			failcnt++;
+			printf("0x%x is 0x%x; should 0x%x\r\n", (__u32)pmem, *pmem, (__u32)0);
+		}
+		pmem++;
+		if ( prcss++ > prcss_cmp)
+		{
+			prcss = 0;
+			puts(".");
+		}	
+	}
+	if ( failcnt)
+		printf("Test failed: %d errors\r\n", failcnt);
+	else
+		puts("Test success\r\n");
+	failcnt = 0;
+
+	puts("Fill u32ptr = 0xAAAA5555 5555AAAA\r\n");
+	pmem = (__u32*)CONFIG_SYS_SDRAM_BASE;
+	cnt = PHYS_SDRAM_SIZE/(2*sizeof(__u32));
+	prcss_cmp = cnt / PRCSS_CMP;
+	prcss = 0;
+	while(cnt--)
+	{
+		*pmem++ = (__u32)0xAAAA5555;
+		*pmem++ = (__u32)0x5555AAAA;
+		if ( prcss++ > prcss_cmp)
+		{
+			prcss = 0;
+			puts(".");
+		}	
+	}	
+	puts("Check:\r\n");
+	pmem = (__u32*)CONFIG_SYS_SDRAM_BASE;
+	cnt = PHYS_SDRAM_SIZE/(2*sizeof(__u32));
+	prcss_cmp = cnt / PRCSS_CMP;
+	prcss = 0;
+	while(cnt--)
+	{
+		if (*pmem != (__u32)0xAAAA5555)
+		{
+			failcnt++;
+			printf("0x%x is 0x%x; should 0x%x\r\n", (__u32)pmem, *pmem, (__u32)0xAAAA5555);
+		}
+		pmem++;
+		if (*pmem != (__u32)0x5555AAAA)
+		{
+			failcnt++;
+			printf("0x%x is 0x%x; should 0x%x\r\n", (__u32)pmem, *pmem, (__u32)0x5555AAAA);
+		}
+		pmem++;
+		if ( prcss++ > prcss_cmp)
+		{
+			prcss = 0;
+			puts(".");
+		}		
+	}
+	if ( failcnt)
+		printf("Test failed: %d errors\r\n", failcnt);
+	else
+		puts("Test success\r\n");
+	failcnt = 0;
+
+
+}
+#endif
+/* ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
+
+void spl_board_init(void)
+{
+#ifndef CONFIG_SPL_USB_SDP_SUPPORT
+	/* Serial download mode */
+	if (is_usb_boot()) {
+		puts("Back to ROM, SDP\n");
+		restore_boot_params();
+	}
+#endif
+
+	init_usb_clk();
+
+	puts("Normal Boot\n");
+}
+
+#ifdef CONFIG_SPL_LOAD_FIT
+int board_fit_config_name_match(const char *name)
+{
+	/* Just empty function now - can't decide what to choose */
+	debug("%s: %s\n", __func__, name);
+
+	return 0;
+}
+#endif
+
+void board_init_f(ulong dummy)
+{
+	int ret;
+
+	/* Clear global data */
+	memset((void *)gd, 0, sizeof(gd_t));
+
+	arch_cpu_init();
+
+	init_uart_clk(0); /* Init UART0 clock */
+
+	board_early_init_f();
+
+	timer_init();
+
+	preloader_console_init();
+
+	/* Clear the BSS. */
+	memset(__bss_start, 0, __bss_end - __bss_start);
+
+	ret = spl_init();
+	if (ret) {
+		debug("spl_init() failed: %d\n", ret);
+		hang();
+	}
+
+	enable_tzc380();
+
+	/* Adjust pmic voltage to 1.0V for 800M */
+	setup_i2c(2, CONFIG_SYS_I2C_SPEED, 0x7f, &i2c_pad_info3);
+
+	power_init_board();
+
+	/* DDR initialization */
+	spl_dram_init();
+
+	board_init_r(NULL, 0);
+}
