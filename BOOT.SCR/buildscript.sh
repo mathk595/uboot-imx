45366d748731660161e68d97ff7c04ea1c63558c
philipp <speeth@keith-koep.com>
2020-05-25 08:56:41 +0200

    boot.scr from 18.09

--- BOOT.SCR/buildscript.sh
+++ BOOT.SCR/buildscript.sh
@@ -1,2 +1,7 @@
 #!/bin/sh
-../tools/mkimage -A arm -T script -O linux -d  boot.debian.pConXS-7inch.txt boot.debian.pConXS-7inch.scr
+../tools/mkimage -A arm -T script -O linux -d  boot.debian.pConXS-7inch.txt             boot.debian.pConXS-7inch.scr
+../tools/mkimage -A arm -T script -O linux -d  boot.debian.pConXS-7inch-fat.txt         boot.debian.pConXS-7inch-fat.scr
+../tools/mkimage -A arm -T script -O linux -d  boot.debian.pConXS-7inch-fat-debug.txt   boot.debian.pConXS-7inch-fat-debug.scr
+../tools/mkimage -A arm -T script -O linux -d  boot.debian.pConXS-7inch-ext4.txt        boot.debian.pConXS-7inch-ext4.scr
+../tools/mkimage -A arm -T script -O linux -d  boot.debian-installer.txt                boot.debian-installer.scr
+../tools/mkimage -A arm -T script -O linux -d  boot.debian-installer-ext4.txt           boot.debian-installer-ext4.scr
